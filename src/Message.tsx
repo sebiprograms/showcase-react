44b46748fd7cf354e800bf6a49d206de5e220ff8
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-01-14 00:47:57 -0600

    Testing conditions in JSX
    Testing out JSX conditions in my new react
    component I created last commit. This code should
    display two text depending on condition if working
    properly.

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,9 @@
 
 function Message() {
   // JSX: JavaScript XML aka code gets compiled to javascript
+  const name = 'Mosh';
+  if (name)
+    return <h1>Hello {name}</h1>;
   return <h1>Hello World</h1>;
 }
 
