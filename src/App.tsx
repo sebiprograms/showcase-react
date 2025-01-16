305d7ddeed30ae0ced75d93dd5f2b373a13e083b
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-01-15 22:13:20 -0600

    Button component
    Commit creates a button component for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button />
     </div>
   );
 }
