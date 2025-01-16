5bdbd19c8da63781385cb2fafb603233108416be
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-01-15 21:42:23 -0600

    handleselectItem
    Created a new function in App.tsx that console
    logs when click something in the list item.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,18 @@ import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   // components take arguments like html takes a tag
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
