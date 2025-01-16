f321a92e0088b0d7b2156e2575ef8e60b3f58d58
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-01-15 20:58:31 -0600

    ListGroup component edit
    With listgroup I'm learning how to add dynamic
    code to my webpage. The goal is to create a
    interactive list.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,20 @@
 function ListGroup() {
-  return <h1>List Group</h1>
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
